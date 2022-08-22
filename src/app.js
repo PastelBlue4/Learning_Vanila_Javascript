53aff9a7e9c607bde1c048b1f440fedec13b93cc
Romuru <pastelblue0721@gmail.com>
2022-08-22 23:08:51 +0900

    upload file :)

---
+++ src/app.js
@@ -0,0 +1,50 @@
+// function numberfilter(value, valueIndex, oldArray) {
+//   // console.log(value);
+//   // console.log(`I'm index ${valueIndex}`);
+//   return value !== testCaseArray[valueIndex + 1];
+// }
+
+// // console.log(testCaseArray);
+// console.log(testCaseArray);
+// console.log(testCaseArray.filter(numberfilter));
+
+// const array = [1, 2, 2, 3, 3, 3, 4, 4, 4, 4, 5, 5, 5, 5, 5];
+// const array1 = [1, 2, 2, 2, 2, 2, 2, 2, 3, 3, 3, 3, 3, 3, 3, 4, 4, 4, 4, 4];
+// const array2 = [1, 2, 2, 3, 3, 1, 2, 3, 4, 2, 2, 1, 1, 2, 2, 4, 5, 6, 6, 2, 2];
+
+// // function add(a, b) {
+// //   console.log(a + b);
+// //   return false;
+// // }
+
+// const overlapFilter = (currentValue, indexOfValue, oldArray) => {
+//   return currentValue !== oldArray[indexOfValue + 1];
+// };
+
+// console.log(array.filter(overlapFilter));
+// console.log(array1.filter(overlapFilter));
+// console.log(array2.filter(overlapFilter));
+
+// function testing() {
+//   console.log("hello!");
+//   return null;
+// }
+
+// const container = testing;
+
+// console.log(container);
+
+// function replaceIndex(oldArray, indexOfOldValue, newValue) {
+//   const frontPart = oldArray.slice(0, indexOfOldValue);
+//   console.log(frontPart);
+//   const backendPart = oldArray.slice(indexOfOldValue + 1, 6);
+//   console.log(backendPart);
+//   const newArray = [...frontPart, newValue, ...backendPart];
+//   return newArray;
+// }
+
+// const testArray = ["1", "2", "3", "4", "5", "6", "7"];
+
+// console.log(replaceIndex(testArray, 2, "1"));
+
+// 버튼을 누르면 그 버튼안에 있는 내용을 특정한 값으로 바꾸기.
